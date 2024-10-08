dc80ea49441362a89caefe8f4271657dfe06ee6d
Leon Sorokin <leeoniya@gmail.com>
2024-10-08 12:41:40 -0500

    updates

--- examples/react/table/src/main.tsx
+++ examples/react/table/src/main.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
+import { useRef, useState } from 'react'
 import { createRoot } from 'react-dom/client'
+import { inferSchema, initParser } from 'udsv'
 
 import { useVirtualizer } from '@tanstack/react-virtual'
 import {
@@ -8,90 +10,114 @@ import {
   getSortedRowModel,
   useReactTable,
 } from '@tanstack/react-table'
-import { makeData } from './makeData'
 import type { ColumnDef, Row, SortingState } from '@tanstack/react-table'
-import type { Person } from './makeData'
 import './index.css'
 
-function ReactTableVirtualized() {
+interface Table {
+  cols: Array<ColumnDef<any>>
+  data: Array<Record<string, unknown>>
+}
+
+interface CSVDropperProps {
+  setData: (table: Table | null) => void
+}
+
+function CSVDropper(props: CSVDropperProps) {
+  const onDrop = (e: React.DragEvent) => {
+    e.preventDefault()
+
+    for (const item of e.dataTransfer.items) {
+      if (item.kind == 'file') {
+        const file = item.getAsFile()!
+
+        if (file.name.endsWith('.csv')) {
+          file.text().then((text) => {
+            console.time('parse')
+
+            const s = inferSchema(text)
+            const p = initParser(s)
+            const d = p.typedObjs(text)
+
+            console.timeEnd('parse')
+
+            const cols: Array<ColumnDef<any>> = s.cols.map((c) => ({
+              header: c.name,
+              accessorKey: c.name,
+            }))
+
+            props.setData({ cols, data: d })
+          })
+        }
+      }
+    }
+  }
+
+  const onDragOver = (e: React.DragEvent) => {
+    e.preventDefault()
+  }
+
+  return (
+    <div
+      style={{
+        width: '600px',
+        height: '600px',
+        background: 'pink',
+        userSelect: 'none',
+      }}
+      onDrop={onDrop}
+      onDragOver={onDragOver}
+    >
+      Drag/drop CSV here...
+    </div>
+  )
+}
+
+// Table using insane amount of memory even with virtualisation when scrolling #5696
+// https://github.com/TanStack/table/issues/5696
+
+function ReactTableVirtualized({ table }: { table: Table }) {
   const [sorting, setSorting] = React.useState<SortingState>([])
 
-  const columns = React.useMemo<Array<ColumnDef<Person>>>(
-    () => [
-      {
-        accessorKey: 'id',
-        header: 'ID',
-        size: 60,
-      },
-      {
-        accessorKey: 'firstName',
-        cell: (info) => info.getValue(),
-      },
-      {
-        accessorFn: (row) => row.lastName,
-        id: 'lastName',
-        cell: (info) => info.getValue(),
-        header: () => <span>Last Name</span>,
-      },
-      {
-        accessorKey: 'age',
-        header: () => 'Age',
-        size: 50,
-      },
-      {
-        accessorKey: 'visits',
-        header: () => <span>Visits</span>,
-        size: 50,
-      },
-      {
-        accessorKey: 'status',
-        header: 'Status',
-      },
-      {
-        accessorKey: 'progress',
-        header: 'Profile Progress',
-        size: 80,
-      },
-      {
-        accessorKey: 'createdAt',
-        header: 'Created At',
-        cell: (info) => info.getValue<Date>().toLocaleString(),
-      },
-    ],
-    [],
-  )
-
-  const [data, setData] = React.useState(() => makeData(50_000))
-
-  const table = useReactTable({
-    data,
-    columns,
+  const table2 = useReactTable({
+    data: table.data,
+    columns: table.cols,
     state: {
       sorting,
     },
     onSortingChange: setSorting,
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
-    debugTable: true,
+    enableColumnResizing: false,
+    defaultColumn: {
+      // minSize: 100,
+      // maxSize: 1000,
+      size: 170,
+    },
   })
 
-  const { rows } = table.getRowModel()
+  const { rows } = table2.getRowModel()
 
-  const parentRef = React.useRef<HTMLDivElement>(null)
+  const parentRef = useRef<HTMLDivElement>(null)
 
   const virtualizer = useVirtualizer({
     count: rows.length,
     getScrollElement: () => parentRef.current,
-    estimateSize: () => 34,
+    estimateSize: () => 17.33,
     overscan: 20,
   })
 
   return (
     <div ref={parentRef} className="container">
       <div style={{ height: `${virtualizer.getTotalSize()}px` }}>
-        <table>
+        <table
+          {...{
+            style: {
+              width: table2.getCenterTotalSize(),
+            },
+          }}
+        >
           <thead>
-            {table.getHeaderGroups().map((headerGroup) => (
+            {table2.getHeaderGroups().map((headerGroup) => (
               <tr key={headerGroup.id}>
                 {headerGroup.headers.map((header) => {
                   return (
@@ -159,26 +185,13 @@ function ReactTableVirtualized() {
 }
 
 function App() {
-  return (
-    <div>
-      <p>
-        For tables, the basis for the offset of the translate css function is
-        from the row's initial position itself. Because of this, we need to
-        calculate the translateY pixel count different and base it off the the
-        index.
-      </p>
-      <ReactTableVirtualized />
-      <br />
-      <br />
-      {process.env.NODE_ENV === 'development' ? (
-        <p>
-          <strong>Notice:</strong> You are currently running React in
-          development mode. Rendering performance will be slightly degraded
-          until this application is build for production.
-        </p>
-      ) : null}
-    </div>
-  )
+  const [table, setTable] = useState<Table | null>(null)
+
+  if (table == null) {
+    return <CSVDropper setData={setTable} />
+  }
+
+  return <ReactTableVirtualized table={table} />
 }
 
 const container = document.getElementById('root')
